83a08e1d6d1665e52ea755e271af0e05a129647d
Bineesh Nadukkalam <bineesh.nadukkalam@alludo.com>
2023-07-04 13:30:54 +0400

    Add ipsw support
    With this change, users can specify ipsw config to initialize macvms.
    Make sure that the boot wait should include installation time also.

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/hashicorp/packer-plugin-sdk/plugin"
 
+	"github.com/Parallels/packer-plugin-parallels/builder/parallels/ipsw"
 	"github.com/Parallels/packer-plugin-parallels/builder/parallels/iso"
 	"github.com/Parallels/packer-plugin-parallels/builder/parallels/macvm"
 	"github.com/Parallels/packer-plugin-parallels/builder/parallels/pvm"
@@ -21,6 +22,7 @@ func main() {
 	pps.RegisterBuilder("iso", new(iso.Builder))
 	pps.RegisterBuilder("pvm", new(pvm.Builder))
 	pps.RegisterBuilder("macvm", new(macvm.Builder))
+	pps.RegisterBuilder("ipsw", new(ipsw.Builder))
 	err := pps.Run()
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
